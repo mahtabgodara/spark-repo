2f59e5c3850b1047197522f5c6ea7be8f86360ef
mahtab.singh <mahtab.singh@guavus.com>
2015-05-24 09:56:43 +0530

    Added Streaming Example

--- examples/src/main/scala/com/training/spark/App.scala
+++ examples/src/main/scala/com/training/spark/App.scala
@@ -6,15 +6,14 @@ import org.apache.spark._
 object App {
   
   def main(args : Array[String]) {
-    val logFile = "/home/hduser/projects/spark/README.md" // Should be some file on your system
-    val sc = new SparkContext("local", "Simple App", "/home/hduser/projects/spark",
-    List("/home/hduser/projects/sparkProject/examples/target/examples-0.0.1.jar"))
-    //val conf = new SparkConf().setAppName("SimpleApp")
-    //val sc = new SparkContext(conf)
+    val logFile = args(0) 
+    val conf = new SparkConf().setAppName("SimpleApp")
+    val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+//    sc.close()
   }
 
 }

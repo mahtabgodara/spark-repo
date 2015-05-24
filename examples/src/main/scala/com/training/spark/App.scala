daf199712c8fc943a86e400a41ed7e1ac65a50a8
mahtab.singh <mahtab.singh@guavus.com>
2015-05-24 21:17:48 +0530

    updated example for spark 1.2

--- examples/src/main/scala/com/training/spark/App.scala
+++ examples/src/main/scala/com/training/spark/App.scala
@@ -1,19 +1,18 @@
 package com.training.spark.examples
 import org.apache.spark._
-/**
- * @author ${user.name}
- */
+
 object App {
   
   def main(args : Array[String]) {
     val logFile = args(0) 
     val conf = new SparkConf().setAppName("SimpleApp")
+    conf.setMaster("local")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
-//    sc.close()
+   // sc.stop
   }
 
 }
---
+++ examples/src/main/scala/com/training/spark/CassandraExample.scala
@@ -0,0 +1,17 @@
+package com.training.spark.examples
+import org.apache.spark._
+import com.datastax.spark.connector._
+
+object CassandraExample {
+  
+  def main(args : Array[String]) {
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
+    val sc = new SparkContext("local[2]", "test", conf)
+    val rdd = sc.cassandraTable("test", "kv")
+    println(rdd.count)
+    println(rdd.first)
+    //println(rdd.map(_.getInt("value")).sum)
+    val collection = sc.parallelize(Seq(("key3", 3), ("key4", 4)))
+    collection.saveToCassandra("test", "kv", SomeColumns("key", "value"))  
+  }
+}
